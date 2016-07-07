de0ec4e92731df1c0e0196c93a8b2c29956d726e
froginfrog <wrx_1984@163.com>
2016-07-07 16:42:59 +0800

    update DBclass
    update DBclass

--- libs/Common/DB.class.php
+++ libs/Common/DB.class.php
@@ -1,12 +1,21 @@
 <?php
 class DB {
-    public static $dbConfig = [];
-    public static $link = null;
-    public static $flag = false;
-    
-    public function __construct(){
+    private $dbConfig = [];
+    private $link = null;
+    private static $instance = null;
+
+    public static function getInstance(){
+        if(is_null(self::$instance)){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __clone() {}
+
+    private function __construct(){
         global $config;
-        self::$dbConfig = [
+        $this->dbConfig = [
             'hostname' => $config['DB_HOST'],
             'username' => $config['DB_USER'],
             'password' => $config['DB_PWD'],
@@ -15,17 +24,15 @@ class DB {
             'charset'  => $config['DB_CHARSET'],
             'dsn'      => $config['DB_TYPE'].':host='.$config['DB_HOST'].';dbname='.$config['DB_NAME'].';charset='.$config['DB_CHARSET']
         ];
-        if(!self::$flag){
-            $dbConfig = self::$dbConfig;
-            try{
-                self::$link = new PDO($dbConfig['dsn'], $dbConfig['username'], $dbConfig['password']);
+        $dbConfig = $this->dbConfig;
+        try {
+            $this->link = new PDO($dbConfig['dsn'], $dbConfig['username'], $dbConfig['password']);
+            $this->link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            }catch (PDOException $e){
-                $this->err($e);
-            }
-            self::$link ->exec('set names'.$dbConfig['charset']);
-            self::$flag = true;
+        } catch (PDOException $e) {
+            $this->err($e);
         }
+        $this->link->exec('set names ' . $dbConfig['charset']);
     }
 
     public function __destruct(){
@@ -38,9 +45,9 @@ class DB {
      * @return string
      */
     public function quote($str){
-        return self::$link->quote($str);
+        return $this->link->quote($str);
     }
-
+    /*
     private function parseCondition($arr){
         $str = "";
         foreach($arr as $k=>$v){
@@ -52,55 +59,121 @@ class DB {
             $str .= $sep."`".$k."`"."='".$v."'";
         }
         return $str;
+    }*/
+    /**
+     * 为bindValue生成带占位符的字符串
+     * @param $arr
+     * @return string
+     */
+    private function parseCondition($arr){
+        $str = "";
+        foreach($arr as $k=>$v){
+            if($str == ""){
+                $sep = "";
+            }else{
+                $sep = ",";
+            }
+            $str .= $sep."`".$k."`"."=:".$k;
+        }
+        return $str;
     }
 
     public function count($sql){
-        $stmt = self::$link->prepare($sql);
+        $stmt = $this->link->prepare($sql);
         $stmt ->execute();
         return $stmt->fetchColumn();
     }
 
     public function getAll($sql){
-        $stmt = self::$link -> prepare($sql);
+        $stmt = $this->link -> prepare($sql);
         $stmt ->execute();
         $res = $stmt -> fetchAll(PDO::FETCH_ASSOC);
         return $res;
     }
 
     public function getOne($sql){
-        $stmt = self::$link -> prepare($sql);
+        $stmt = $this->link -> prepare($sql);
         $stmt ->execute();
         $res = $stmt -> fetch(PDO::FETCH_ASSOC);
         return $res;
     }
 
-    // insert into table (col1, col2, col3) values ('v1', 'v2', 'v3')
+    /**
+     * insert into table (col1, col2, col3) values ('v1', 'v2', 'v3')
+     * @param string $table
+     * @param array $arr
+     */
     public function insert($table, $arr){
         $keys = "`". join("`,`", array_keys($arr)) ."`";
-        $values = "'". join("','", array_values($arr)) ."'";
-        $sql = "insert into $table ($keys) vlaues ($values)";
-        $stmt = self::$link -> prepare($sql);
+        //$values = "'". join("','", array_values($arr)) ."'";
+        $placeHolder = '';
+        foreach ($arr as $k=>$v){
+            if($placeHolder == ''){
+                $sep = ':';
+            }else{
+                $sep = ',:';
+            }
+            $placeHolder .= $sep.$k;
+        }
+        $sql = "insert into `$table` ($keys) values ($placeHolder)";
+        $stmt = $this->link -> prepare($sql);
+        foreach($arr as $key=>$value){
+            $stmt->bindValue(':'.$key, $value);
+        }
         $stmt -> execute();
-        return self::$link -> lastInsertId();
+        //return $this->link -> lastInsertId();
     }
 
-    //update table set col1=v1, col2=v2 where blabla
+    /**
+     * update table set col1=v1, col2=v2 where blabla
+     * @param string $table
+     * @param array $arr
+     * @param array $where
+     * @return int
+     */
     public function update($table, $arr, $where){
         $condition = $this->parseCondition($arr);
-        $where = " where $where";
-        $sql = "update $table set $condition $where";
-        $stmt = self::$link -> prepare($sql);
+        $_where = $this->parseCondition($where);
+        $_where = ' where '.$_where;
+        $bind = array_merge($arr, $where);
+        $sql = "update `$table` set $condition $_where";
+        $stmt = $this->link -> prepare($sql);
+        foreach($bind as $key=>$value){
+            $stmt->bindValue(':'.$key, $value);
+        }
         $stmt -> execute();
         return $stmt->rowCount();
     }
 
-    // delete from table where blabla
+    /**
+     * delete from table where blabla
+     * @param string $table
+     * @param array $arr
+     * @return int
+     */
     public function delete($table, $arr){
         $where = $this->parseCondition($arr);
         $sql = "delete from $table where $where";
-        $stmt = self::$link -> prepare($sql);
+        $stmt = $this->link -> prepare($sql);
+        foreach($arr as $key=>$value){
+            $stmt->bindValue(':'.$key, $value);
+        }
         $stmt -> execute();
         return $stmt -> rowCount();
+    }
+
+    /**
+     * 执行复杂的update、insert、delete时可以调用这个方法。
+     * @param string $sql
+     * @return int
+     */
+    public function doSql($sql){
+        $count = $this->link -> exec($sql);
+        return $count;
+    }
+
+    public function lastInsertId(){
+        return $this->link->lastInsertId();
     }
 
     private function err($err){
@@ -108,6 +181,6 @@ class DB {
     }
 
     private function close(){
-        self::$link = null;
+        $this->link = null;
     }
 }
--- libs/Core/Model/indexModel.class.php
+++ libs/Core/Model/indexModel.class.php
@@ -1,14 +1,14 @@
 <?php
 class indexModel {
     function index(){
-        $data = new DB();
+        $data = DB::getInstance();
         $sql = "select count(*) from user limit 10";
         $res = $data->count($sql);
         return $res;
     }
     
     function user(){
-        $data = new DB();
+        $data = DB::getInstance();
         $id = $data->quote($_GET['id']);
         $name = $data->quote($_GET['name']);
         $sql = "select * from user where id=$id and username=$name";
