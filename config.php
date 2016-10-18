2edbf9b3ce388790bd36f9f9ddb30dafa9f6fe0f
froginfrog <wrx_1984@163.com>
2016-10-18 11:12:48 +0800

    增加时区设置

--- config.php
+++ config.php
@@ -1,5 +1,12 @@
 <?php
 return array(
+    //时区设置
+    'TIMEZONE'            => 'Asia/Shanghai',
+    //网站所在目录
+    'ROOT'                => '/jisuan/',
+    //上传文件存放位置
+    'UPLOAD'              => './uploads',
+
     //smarty配置
     'SM_LEFT_DELIMITER'   => '{',
     'SM_RIGHT_DELIMITER'  => '}',
@@ -8,16 +15,20 @@ return array(
     'CAHCE_DIR'           => './templates/cache',
     'CACHEING'            => false,
     'CACHE_LIFETIME'      => 120,
+    'STATICROOT'          => '',
 
     //mysql配置
     'DB_HOST'             => 'localhost',
     'DB_USER'             => 'root',
     'DB_PWD'              => '',
-    'DB_NAME'             => 'mydb',
+    'DB_NAME'             => 'jisuan',
     'DB_PORT'             => '3306',
     'DB_TYPE'             => 'mysql',
     'DB_CHARSET'          => 'utf8',
 
     //水印图片存放位置
-    'watermark'               => './static/images/shuiyin.png'
+    'watermark'           => './static/images/shuiyin.png',
+
+    //后台产品列表每页个数
+    'adminProPageSize'    => 20,
 );
--- index.php
+++ index.php
@@ -3,7 +3,8 @@ require_once './libs/function.php';
 spl_autoload_register('autoload');
 $config = include 'config.php';
 $router = include 'url.php';
+date_default_timezone_set($config['TIMEZONE']);
 session_start();
 
 $start = new router();
-$start -> match($_GET['params'], $router);
+$start -> match(isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO']: "", $router);
